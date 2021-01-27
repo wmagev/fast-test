d7a56a080274e73a243402d468e3e8dac1adf043
willie <willie.lee1020@outlook.com>
2021-01-27 11:39:45 -0500

    Configure prettier

---
+++ src/__test__/containers/Search.test.js
@@ -0,0 +1,7 @@
+import { render, fireEvent } from '../../test-utils'
+import * as customQueries from '../../test-utils/custom-queries'
+import Search from '../../containers/Search'
+
+test('render Search component', () => {
+    render(<Search />)
+})
